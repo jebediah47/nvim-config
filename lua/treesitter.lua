945b77f216ca80723f59e17cb95e7cc8996ffb79
jebediah47 <jebediah47@protonmail.com>
2022-07-08 12:25:00 +0300

    Added nvim-treesitter config

---
+++ lua/treesitter.lua
@@ -0,0 +1,16 @@
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all"
+  ensure_installed = { "c", "lua", "cpp", "typescript", "vim", "css", "html", "javascript", "astro", "go", "bash", "svelte" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  auto_install = true,
+
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+    additional_vim_regex_highlighting = true,
+  },
+}
