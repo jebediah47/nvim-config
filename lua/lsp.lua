a28c1b8101aef5a8f7c3dab61a5e22c02e38d5a0
jebediah47 <jebediah47@protonmail.com>
2022-07-08 11:35:49 +0300

    Fortmatted lsp.lua

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -12,16 +12,22 @@ lspconfig.gopls.setup {}
 
 -- Webdev config
 -- JavaScript/TypeScript LSP server
-lspconfig.tsserver.setup {}
+lspconfig.tsserver.setup {
+    filetypes = { "typescript", "javascript" }
+}
 
 -- Astro LSP server
-lspconfig.astro.setup {}
+lspconfig.astro.setup {
+    filetypes = { "astro" }
+}
 
 -- TailwindCSS LSP server
 lspconfig.tailwindcss.setup {}
 
 -- Svelte LSP server
-lspconfig.svelte.setup {}
+lspconfig.svelte.setup {
+    filetypes = { "svelte" }
+}
 
 -- ESLint config
 lspconfig.eslint.setup {}
@@ -30,29 +36,29 @@ local cmp = require'cmp'
 
 cmp.setup({
   snippet = {
-      -- REQUIRED - you must specify a snippet engine
-      expand = function(args)
-        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-      end,
-    },
-  window = {
-      -- completion = cmp.config.window.bordered(),
-      -- documentation = cmp.config.window.bordered(),
-    },
-  mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  }),
-  sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      { name = 'luasnip' }, -- For luasnip users.
-    }, {
-      { name = 'buffer' },
-    })
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+    end,
+  },
+window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+},
+mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+}),
+sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' }, -- For luasnip users.
+  }, {
+    { name = 'buffer' },
   })
+})
 
   -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
   cmp.setup.cmdline('/', {
@@ -63,18 +69,12 @@ cmp.setup({
   })
 
   -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline(':', {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-      { name = 'path' }
-    }, {
-      { name = 'cmdline' }
-    })
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
   })
-
-  -- Setup lspconfig.
-  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-  require('lspconfig')['eslint'].setup {
-  capabilities = capabilities
-}
+})
 
